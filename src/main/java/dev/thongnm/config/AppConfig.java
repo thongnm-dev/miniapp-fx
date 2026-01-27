bd3c475ef5040e5473a794a456684a5411e01df8
thongnm-dev <thongnm.dev@gmail.com>
2026-01-27 11:27:23 +0700

    update config

--- src/main/java/dev/thongnm/config/AppConfig.java
+++ src/main/java/dev/thongnm/config/AppConfig.java
@@ -1,7 +1,8 @@
 package dev.thongnm.config;
 
-import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import lombok.Data;
 
 @Data
 @ConfigurationProperties(prefix = "app")
--- src/main/java/dev/thongnm/config/BacklogConfig.java
+++ src/main/java/dev/thongnm/config/BacklogConfig.java
@@ -1,9 +1,11 @@
 package dev.thongnm.config;
 
-import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import lombok.Data;
 
 @Data
 @ConfigurationProperties(prefix = "backlog")
 public class BacklogConfig {
+    private String api;
 }
